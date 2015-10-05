3187e1075b375f55e54131363181a2a69be28485
Kajetan <invert14@gmail.com>
2015-10-05 21:56:42 +0200

    - monitoring - adding to file

--- CurrencyExchangeRateEntry.py
+++ CurrencyExchangeRateEntry.py
@@ -2,18 +2,20 @@ import Utilities
 from Utilities import parse_to_float
 
 
-class CurrencyExchangEntry:
-    currency = ''
+class CurrencyExchangeRateEntry:
+    name = ''
     buy_ratio = 1.0
     sell_ratio = 1.0
     time_stamp = ''
 
     def __init__(self, name, buy, sell, time_stamp):
-        self.currency = name[0:3]
+        self.name = name[0:3]
         self.buy_ratio = parse_to_float(buy)
         self.sell_ratio = parse_to_float(sell)
         self.time_stamp = time_stamp
 
-    def printData(self):
-        print "%s:  %.4f %.4f   @%s" % (self.currency, self.buy_ratio, self.sell_ratio, self.time_stamp)
+    def get_pretty_data(self):
+        return "%s:  %.4f %.4f  %s" % (self.name, self.buy_ratio, self.sell_ratio, self.time_stamp)
 
+    def print_data(self):
+        print self.get_pretty_data()
--- Monitor.py
+++ Monitor.py
@@ -1,16 +1,46 @@
-from CurrencyExchangEntry import CurrencyExchangEntry
-from bs4 import BeautifulSoup
-import graphy
 import urllib2
+import time
+import os
+from bs4 import BeautifulSoup
+from CurrencyExchangeRateEntry import CurrencyExchangeRateEntry
 
 CURRENCY_STATS_URL = 'https://cinkciarz.pl/kantor/kursy-walut-cinkciarz-pl'
+RATES_OVER_TIME_FILE = "rates.txt"
 
-soup = BeautifulSoup(urllib2.urlopen(CURRENCY_STATS_URL).read(), 'html.parser')
-for row in soup('table')[0].tbody('tr'):
-    cells = row('td')
-    entry = CurrencyExchangEntry(
-        cells[0].span.string,
-        cells[1].string,
-        cells[2].string,
-        cells[5].string)
-    entry.printData()
+rates = dict()
+
+# if os.path.isfile(RATES_OVER_TIME_FILE):
+#     os.remove(RATES_OVER_TIME_FILE)
+
+def check_for_changed_rates():
+    soup = BeautifulSoup(urllib2.urlopen(CURRENCY_STATS_URL).read(), 'html.parser')
+    global row, cells, entry
+    for row in soup('table')[0].tbody('tr'):
+        cells = row('td')
+        entry = CurrencyExchangeRateEntry(
+            cells[0].span.string,
+            cells[1].string,
+            cells[2].string,
+            cells[5].string)
+        entry.print_data()
+        if not rates.has_key(entry.name):
+            rates[entry.name] = list()
+        rates_for_entry = rates[entry.name]
+        last_time_stamp = '00'
+        if len(rates_for_entry) > 0:
+            last_time_stamp = rates_for_entry[-1].time_stamp
+        if entry.time_stamp != last_time_stamp:
+            print 'change!!!!'
+            rates[entry.name].append(entry)
+            write_row_to_file(entry.get_pretty_data())
+    write_row_to_file('')
+
+
+def write_row_to_file(row):
+    with open(RATES_OVER_TIME_FILE, "a") as myfile:
+        myfile.write("%s\n" % row)
+
+
+while True:
+    check_for_changed_rates()
+    time.sleep(30)
